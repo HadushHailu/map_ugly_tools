c357b5ef83e61d9cb53c9b8b0b70a6c6c41b224b
Hadush <had.hailu@gmail.com>
2022-02-05 22:54:53 +0900

    delete obstacle within rect

--- scripts/delete_obstacle_within_rect.py
+++ scripts/delete_obstacle_within_rect.py
@@ -1,117 +1,97 @@
 #!/usr/bin/env python
 import rospy
 import numpy as np
-from nav_msgs.msg import OccupancyGrid 
-from visualization_msgs.msg import Marker
+from nav_msgs.msg import OccupancyGrid
+from map_ugly_tools.msg import DeleteRectObstacle
 
-class FirstObstacleCellEtc():
+class DeleteObstacleWithinRect():
     def __init__(self):
         # var
-        self.rviz_pos_x = None
-        self.rviz_pos_y = None
         self.resolution = None
         self.width = None
         self.height = None
         self.data = None
-        self.origin_x = None
-        self.origin_y = None
+        self.origin = None
+   
+
+        self.new_map = OccupancyGrid()
 
         # init
-        rospy.init_node("first_obstacle_cell_etc", anonymous=True)
+        rospy.init_node("delete_obstacle_with_rect", anonymous=True)
 
         # sub/pub
-        self.sphere_marker_pub = rospy.Publisher("/fisrt_obstacle_cell_marker",Marker,queue_size=10)
+        self.map_rect_pub = rospy.Publisher("/map_rect", OccupancyGrid,queue_size=10)
         self.map_sub = rospy.Subscriber("/map",OccupancyGrid,self.map_callback)
-
+        self.rect_sub = rospy.Subscriber("/rect",DeleteRectObstacle, self.delete_rect_obstacle_callback)
+        
     def map_callback(self,msg):
         self.resolution = msg.info.resolution
         self.width = msg.info.width
         self.height = msg.info.height
+        self.origin = msg.info.origin
         self.data = msg.data
-        self.origin_x = msg.info.origin.position.x
-        self.origin_y = msg.info.origin.position.y
+        self.new_map = msg
 
         rospy.loginfo("Map info: resolution: {0}meter/pixel width:{1}pixels height: {2}pixels data_size: {3}pixels".format(self.resolution,self.width,self.height,len(self.data)))
 
-        self.mark_the_first_occupied_cell()
-        self.mark_all_occupied_cells()
+
+    def delete_rect_obstacle_callback(self, msg):
+        #..
+        rospy.loginfo("new rect data arrived!")
+        point_1_x = msg.point_1.x
+        point_1_y = msg.point_1.y
+        point_2_x = msg.point_2.x
+        point_2_y = msg.point_2.y
+
+        #..get distance in meter from the bottom left corner of the map
+        point_1_x_d = -1 * ( self.origin.position.x - point_1_x)
+        point_1_y_d = -1 * ( self.origin.position.y - point_1_y)
+        point_2_x_d = -1 * ( self.origin.position.x - point_2_x)
+        point_2_y_d = -1 * ( self.origin.position.y - point_2_y)
+
+        #.. get corresponding pixel values
+        pixel_1 = int((point_1_x_d / self.resolution) * (point_1_y_d / self.resolution))
+        pixel_2 = int((point_2_x_d / self.resolution) * (point_2_y_d / self.resolution))
+
+        pixel_small = pixel_1 if pixel_1 < pixel_2 else pixel_2
+        pixel_large = pixel_1 if pixel_1 > pixel_2 else pixel_2
+
+        #.. 
+        h_gap = int(pixel_small / self.width)
+        w_1 = pixel_small % self.width
+        w_2 = pixel_large % self.width
+        w_gap = w_1 if w_1 < w_2 else w_2
+
+        h_rect = int(pixel_large/ self.width) - h_gap
+        w_rect = (w_1 if w_1 > w_2 else w_2) - w_gap
+
+        rospy.loginfo("h_gap:{0} w_gap:{1} h_rect: {2} w_rect {3}".format(h_gap, w_gap, h_rect, w_rect))
 
 
-    def mark_the_first_occupied_cell(self):
-        #
-        map_data_to_set = set(self.data)
-        rospy.loginfo("Map values: {0}".format(map_data_to_set))
-        
-        index_first_occupied_cell = self.data.index(100)
-        #index_first_occupied_cell = 0
-        rospy.loginfo("Index of first occupied cell in map data: {0}".format(index_first_occupied_cell))
+        #..
+        map_data_np = np.array(self.data)
+        map_original_np = np.array(self.data)
+        for j in range(h_rect):
+            rospy.loginfo("Befor len of occupied cell: {0} j: {1}".format(len(np.where(map_data_np[(h_gap + j) * self.width + w_gap: (h_gap + j) * self.width + w_gap + w_rect] == 100)), j))
+            map_data_np[(h_gap + j) * self.width + w_gap: (h_gap + j) * self.width + w_gap + w_rect] = 100
+            rospy.loginfo("After len of occupied cell: {0} j: {1}".format(len(np.where(map_data_np[(h_gap + j) * self.width + w_gap: (h_gap + j) * self.width + w_gap + w_rect] == 100)), j))
 
-        # The position of the first occupied cell in the map data matrix
-        mat_pos_x = index_first_occupied_cell%self.width
-        mat_pos_y = int(index_first_occupied_cell/self.width) # matrix position couldn't be float value
-        rospy.loginfo("Position of first occupied cell in the map matrix: ({0},{1})".format(mat_pos_x,mat_pos_y))
+        self.new_map.header.frame_id = "map"
+        self.new_map.info.width = self.width
+        self.new_map.info.height = self.height
+        self.new_map.info.origin = self.origin
+        self.new_map.data = map_data_np
 
-        # where is index_of_first_matrix relative to origin/m
-        rviz_pos_x = self.origin_x + mat_pos_x * self.resolution
-        rviz_pos_y = self.origin_x + mat_pos_y * self.resolution
-        rospy.loginfo("Position of first occupied cell in rviz: ({0},{1})".format(rviz_pos_x,rviz_pos_y))
+        rospy.loginfo("Original map number of obstacle pixels: {0}".format(len(np.where(map_original_np == 100)[0])))
+        rospy.loginfo("rect map number of obstacle pixels: {0}".format(len(np.where(map_data_np == 100)[0])))
 
-        # 
-        self.rviz_pos_x = rviz_pos_x
-        self.rviz_pos_y = rviz_pos_y
-
-    def mark_all_occupied_cells(self):
-        #
-        map_to_np = np.array(self.data)
-        filter_occupied_cells = np.where(map_to_np == 100)[0]
-        rospy.loginfo("number of occupied cells: {0})".format(len(filter_occupied_cells)))
-
-    def down_sample_map(self):
-        # get a ration and down sample map
-        pass
-
-    def cut_map_with_obstacle(self):
-        # get map with only the obstacles in it
-        pass
-
-    def insert_obstacle(self):
-        
-        pass
-
-    def delete_obstacle(self):
-        # remove all obstacles from a given rectangle
-        pass
-
-    def inflate_rectangle(self):
-        # inflate all obstacles in the given rectangle
-        pass
-        
-    def sphere_marker(self):
-        marker = Marker()
-        marker.header.frame_id = "map"
-        marker.type = marker.SPHERE
-        marker.action = marker.ADD
-        marker.scale.x = 0.2
-        marker.scale.y = 0.2
-        marker.scale.z = 0.2
-        marker.color.a = 0.6
-        marker.color.r = 1.0
-        marker.color.g = 1.0
-        marker.color.b = 0.0
-        marker.pose.orientation.w = 1.0
-        marker.pose.position.x = self.rviz_pos_x
-        marker.pose.position.y = self.rviz_pos_y
-        marker.pose.position.z = 0.1
-
-        self.sphere_marker_pub.publish(marker)
-        
-
+         
     def run_it(self):
         rate = rospy.Rate(5)
         while not rospy.is_shutdown():
-            self.sphere_marker()
+            self.map_rect_pub.publish(self.new_map)
             rate.sleep()
 
 if __name__ == "__main__":
-    app = FirstObstacleCellEtc()
+    app = DeleteObstacleWithinRect()
     app.run_it()
